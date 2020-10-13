9a415d1b6578b5f54a1e57e4b6d17cedaf000642
Bryce Kennedy <btken88@gmail.com>
2020-10-13 11:43:53 -0600

    Finish section 9 codealong.

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <TheHeader />
+    <button @click="setSelected('active-goals')">Active Goals</button>
+    <button @click="setSelected('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
 export default {
+  components: { TheHeader, ActiveGoals, ManageGoals },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
+  },
+  methods: {
+    setSelected(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
---
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <ErrorAlert v-if="inputIsInvalid">
+        <h2>Input is Invalid!</h2>
+        <p>Please enter some text</p>
+        <button @click="removeError">Okay</button>
+      </ErrorAlert>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: { ErrorAlert },
+  data() {
+    return { inputIsInvalid: false };
+  },
+  methods: {
+    setGoal() {
+      let enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    removeError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
